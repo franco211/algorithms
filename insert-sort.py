68d75b6fb2f70e8ed4ac5eb093d82dc805bb71e6
franco byaruhanga <francisbyaruhanga88@gmail.com>
2023-01-02 17:21:53 -0500

    git commit

--- insert-sort.py
+++ insert-sort.py
@@ -4,9 +4,9 @@ def insert_sort(arr):
         j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
-            j = i - 1
+            j = j - 1
         arr[j+1] = key
 
-arr = [5, 3, 7]
+arr = [5, 3, 7, 6, 9]
 insert_sort(arr)
 print(arr)
