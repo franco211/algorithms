4355c8339fe3893660f7613f22cff1eb582d213d
franco211 <francisbyaruhanga88@gmail.com>
2023-01-09 16:43:16 -0500

    git commit

--- heapify.py
+++ heapify.py
@@ -2,7 +2,7 @@ def max_heapify(heap, heap_size, i):
     left = 2 * i + 1
     right = 2 * i + 2
     largest = i
-    if left < heap_size and heap[left] > heap[largest]:
+    if left <= heap_size and heap[left] > heap[largest]:
         largest = left
     if right < heap_size and heap[right] > heap[largest]:
         largest = right
